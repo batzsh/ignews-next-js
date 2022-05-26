4cb55f9be71fb28a42fe484ac76b57d16bc09e52
Henrique Balmant <henrique.balmant@gmail.com>
2022-05-25 21:39:36 -0300

    feat: add user to faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/fauna";
+
 export default NextAuth({
   providers: [
     GitHubProvider({
@@ -8,4 +11,25 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn(params) {
+      const { email } = params.user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
